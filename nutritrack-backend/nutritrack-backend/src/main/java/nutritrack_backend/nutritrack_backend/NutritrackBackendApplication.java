ec4697027dff45103051ee4eda24a4723b8d1dc4
Isaac Bertonha <isaacbertonha2003@gmail.com>
2025-02-20 00:34:36 -0300

    first commit

---
+++ nutritrack-backend/nutritrack-backend/src/main/java/nutritrack_backend/nutritrack_backend/NutritrackBackendApplication.java
@@ -0,0 +1,13 @@
+package nutritrack_backend.nutritrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutritrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutritrackBackendApplication.class, args);
+	}
+
+}
---
+++ nutritrack-backend/nutritrack-backend/src/test/java/nutritrack_backend/nutritrack_backend/NutritrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nutritrack_backend.nutritrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutritrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
